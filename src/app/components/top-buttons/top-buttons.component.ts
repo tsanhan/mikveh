bdaebd1d9640132d0ab5316f40a1bd0ffcf1b4b3
Victor Yampolsky <Victor.Yampolsky@cust2mate.com>
2024-05-29 23:37:32 +0300

    udate

--- src/app/components/top-buttons/top-buttons.component.ts
+++ src/app/components/top-buttons/top-buttons.component.ts
@@ -50,11 +50,15 @@ export class TopButtonsComponent {
     '?text=%D7%A9%D7%9C%D7%95%D7%9D.%20%D7%90%D7%A0%D7%99%20%D7%A8%D7%95%D7%A6%D7%94%20%D7%9C%D7%A9%D7%90%D7%95%D7%9C%20%D7%A9%D7%90%D7%9C%D7%94%20%D7%91%D7%A0%D7%95%D7%92%D7%A2%20%D7%9C%D7%94%D7%9C%D7%9B%D7%95%D7%AA%20%D7%A0%D7%99%D7%93%D7%94.';
 
   constructor() {
-    this.toggleApproaches.update((prev) => !prev);
     addIcons({ logoWhatsapp });
   }
 
   toggle() {
     this.toggleApproaches.update((prev) => !prev);
+  }
+
+  protected onclose() {
+    console.log('closed');
+
   }
 }
