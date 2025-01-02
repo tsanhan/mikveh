2128773fb57ef0a568bc97081c2ddaa8f25945c5
victor yampolsky <tsanhan@gmail.com>
2025-01-02 21:56:29 +0200

    Update Firebase configuration and hosting target to mikveh4u

--- src/main.ts
+++ src/main.ts
@@ -84,12 +84,12 @@ bootstrapApplication(AppComponent, {
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideFirebaseApp(() =>
       initializeApp({
-        projectId: 'mikveh-bo',
-        appId: '1:136996094637:web:c8c1b0acb8da9f89d9fc24',
-        storageBucket: 'mikveh-bo.firebasestorage.app',
-        apiKey: 'AIzaSyC6AwRpga0-MbVXx3XlkOQFSDpBs9eZ0JQ',
-        authDomain: 'mikveh-bo.firebaseapp.com',
-        messagingSenderId: '136996094637',
+        apiKey: "AIzaSyDGPOUk4AVLY5hoCM0TVI1ZRAsf9Iy2nMw",
+        authDomain: "mikveh-bo.firebaseapp.com",
+        projectId: "mikveh-bo",
+        storageBucket: "mikveh-bo.firebasestorage.app",
+        messagingSenderId: "136996094637",
+        appId: "1:136996094637:web:c8c1b0acb8da9f89d9fc24"
       })
     ),
     provideAuth(() => getAuth()),
