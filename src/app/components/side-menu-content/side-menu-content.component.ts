b88349b5346b05f08678386814741ee06da1f37b
victor yampolsky <tsanhan@gmail.com>
2025-01-15 23:45:24 +0200

    Add WhatsApp button to side menu and update component imports

--- src/app/components/side-menu-content/side-menu-content.component.ts
+++ src/app/components/side-menu-content/side-menu-content.component.ts
@@ -1,23 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { IonIcon, IonSegment, IonSegmentButton, IonButton } from "@ionic/angular/standalone";
+import { IonIcon, IonSegment, IonSegmentButton, IonButton, IonFab, IonFabButton, IonImg } from "@ionic/angular/standalone";
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { bootstrapClock } from '@ng-icons/bootstrap-icons';
-import { heart, timeOutline, searchOutline } from 'ionicons/icons';
+import { timeOutline, searchOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
 @Component({
   selector: 'app-side-menu-content',
   templateUrl: './side-menu-content.component.html',
   styleUrls: ['./side-menu-content.component.scss'],
   standalone: true,
-  imports: [IonButton, IonSegmentButton, IonSegment, IonIcon,NgIcon ],
+  imports: [IonImg, IonFabButton, IonFab, IonButton, IonSegmentButton, IonSegment, IonIcon,NgIcon ],
   viewProviders: [provideIcons({ bootstrapClock })]
 
 
 })
 export class SideMenuContentComponent  implements OnInit {
+  countryCode: string = '972';
+  wsNumber: string = '584298770';
+  url: string =
+    'https://wa.me/' +
+    this.countryCode +
+    this.wsNumber +
+    '?text=%D7%A9%D7%9C%D7%95%D7%9D.%20%D7%90%D7%A0%D7%99%20%D7%A8%D7%95%D7%A6%D7%94%20%D7%9C%D7%A9%D7%90%D7%95%D7%9C%20%D7%A9%D7%90%D7%9C%D7%94%20%D7%91%D7%A0%D7%95%D7%92%D7%A2%20%D7%9C%D7%94%D7%9C%D7%9B%D7%95%D7%AA%20%D7%A0%D7%99%D7%93%D7%94.';
 
   constructor() {
-    addIcons({timeOutline,searchOutline,heart});
+    addIcons({timeOutline,searchOutline});
 
   }
 
