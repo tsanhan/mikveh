cc1f530d7b69f490401b7d1653ee424e65333044
victor yampolsky <tsanhan@gmail.com>
2025-01-16 22:29:29 +0200

    Enhance side menu component with WhatsApp button and improve styling

--- src/app/components/side-menu-content/side-menu-content.component.ts
+++ src/app/components/side-menu-content/side-menu-content.component.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { IonIcon, IonSegment, IonSegmentButton, IonButton, IonFab, IonFabButton, IonImg } from "@ionic/angular/standalone";
+import { ChangeDetectionStrategy, Component, OnInit, signal, WritableSignal } from '@angular/core';
+import { IonIcon, IonSegment, IonSegmentButton, IonButton, IonFab, IonFabButton, IonImg, IonText } from "@ionic/angular/standalone";
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { bootstrapClock } from '@ng-icons/bootstrap-icons';
-import { timeOutline, searchOutline } from 'ionicons/icons';
+import { timeOutline, searchOutline, logoWhatsapp } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-side-menu-content',
   templateUrl: './side-menu-content.component.html',
   styleUrls: ['./side-menu-content.component.scss'],
   standalone: true,
-  imports: [IonImg, IonFabButton, IonFab, IonButton, IonSegmentButton, IonSegment, IonIcon,NgIcon ],
-  viewProviders: [provideIcons({ bootstrapClock })]
+  imports: [CommonModule, IonText, IonImg, IonFabButton, IonFab, IonButton, IonSegmentButton, IonSegment, IonIcon,NgIcon ],
+  viewProviders: [provideIcons({ bootstrapClock })],
+  changeDetection: ChangeDetectionStrategy.OnPush
 
 
 })
 export class SideMenuContentComponent  implements OnInit {
+  page:WritableSignal<"time"|"search"> = signal("time");
+
+
   countryCode: string = '972';
   wsNumber: string = '584298770';
   url: string =
@@ -24,7 +29,7 @@ export class SideMenuContentComponent  implements OnInit {
     '?text=%D7%A9%D7%9C%D7%95%D7%9D.%20%D7%90%D7%A0%D7%99%20%D7%A8%D7%95%D7%A6%D7%94%20%D7%9C%D7%A9%D7%90%D7%95%D7%9C%20%D7%A9%D7%90%D7%9C%D7%94%20%D7%91%D7%A0%D7%95%D7%92%D7%A2%20%D7%9C%D7%94%D7%9C%D7%9B%D7%95%D7%AA%20%D7%A0%D7%99%D7%93%D7%94.';
 
   constructor() {
-    addIcons({timeOutline,searchOutline});
+    addIcons({timeOutline,searchOutline,logoWhatsapp});
 
   }
 
