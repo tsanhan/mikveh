a1199fc88fbf487c3ae8bb1582cc1bdbd27b8b02
victor yampolsky <tsanhan@gmail.com>
2025-01-25 23:04:56 +0200

    Refactor layout and styles for improved consistency and usability in app components

--- src/app/components/day-times/day-times.component.ts
+++ src/app/components/day-times/day-times.component.ts
@@ -1,4 +1,8 @@
+import { AsyncPipe, JsonPipe } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
+import { GeoLocation, Location, Zmanim } from '@hebcal/core';
+
+import { map } from 'rxjs';
 import { LocationService } from 'src/app/services/location.service';
 
 @Component({
@@ -6,15 +10,18 @@ import { LocationService } from 'src/app/services/location.service';
   templateUrl: './day-times.component.html',
   styleUrls: ['./day-times.component.scss'],
   standalone: true,
-  imports: []
+  imports: [AsyncPipe, JsonPipe],
 })
-export class DayTimesComponent  implements OnInit {
-
-  constructor() { }
-  location = inject(LocationService)
-  google = inject(Map)
+export class DayTimesComponent {
+  constructor() {}
+  location = inject(LocationService);
   coordinates = this.location.coordinates;
-
-  ngOnInit() {}
+  zmazim = this.coordinates.pipe(
+    map(({ lat, lng }) => {
+      const loc = new Location(lat, lng, true, 'Asia/Jerusalem');
+      const now = new Date();
+      return new Zmanim(loc, now, false);
+    })
+  );
 
 }
