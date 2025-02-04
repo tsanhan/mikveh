16935448234b36ddd323c90d05e779aed76e840f
victor yampolsky <tsanhan@gmail.com>
2025-02-04 19:15:22 +0200

    Remove legacy Firestore service and optimize data fetching in DalService

--- src/app/services/dal.service.ts
+++ src/app/services/dal.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from '@angular/core';
-import { from, Observable, of, switchMap, tap } from 'rxjs';
+import { from, map, Observable, of, switchMap, tap} from 'rxjs';
 import { IMikveh } from '../interfaces/mikveh.interface';
 import { CacheService } from './cache.service';
 
@@ -9,26 +9,44 @@ import { CacheService } from './cache.service';
 export class DalService {
   cache = inject(CacheService);
 
-  constructor() {}
+  constructor() {
+  }
 
   getMikvehList():Observable<IMikveh[]> {
     return from(this.cache.getMikvehResults()).pipe(
       switchMap((cachedMikvehs: IMikveh[]) => {
-        if (!!cachedMikvehs && cachedMikvehs.length) {
-          return of(cachedMikvehs);
-        }
-
-//https://firestore.googleapis.com/v1/projects/mikveh-bo/databases/(default)/documents/mikvehs
-        // const mikvehsRef = collection(this.afs, 'mikvehs');
-        const mikvehs = of([]);
-        // collectionData(mikvehsRef, {
-        //   idField: 'id',
-        // }) as Observable<IMikveh[]>;
-        return mikvehs.pipe(
-          tap((mikvehs) => this.cache.storeMikvehResults(mikvehs))
-        );
+        if (!!cachedMikvehs && cachedMikvehs.length) return of(cachedMikvehs);
+        const mikvehs = from(fetch('https://firestore.googleapis.com/v1/projects/mikveh-bo/databases/(default)/documents/mikvehs').then((res) => res.json()));
+          return mikvehs.pipe(
+            map(({documents}) => documents),
+            map(docs => docs.map((x:any) => this.documentToJson(x.fields))),
+            tap((mikvehs) => this.cache.storeMikvehResults(mikvehs))
+          )
       })
     );
   }
+
+  documentToJson(fields: any): any {
+    let result: { [key: string]: any } = {};
+    for (let f in fields) {
+        let key = f, value = fields[f],
+            isDocumentType = ['stringValue', 'booleanValue', 'doubleValue',
+                'integerValue', 'timestampValue', 'mapValue', 'arrayValue'].find(t => t === key);
+        if (isDocumentType) {
+            let item = ['stringValue', 'booleanValue', 'doubleValue', 'integerValue', 'timestampValue'].find(t => t === key)
+            if (item)
+                return value;
+            else if ('mapValue' == key)
+                return this.documentToJson(value.fields || {});
+            else if ('arrayValue' == key) {
+                let list = value.values;
+                return !!list ? list.map((l:any) => this.documentToJson(l)) : [];
+            }
+        } else {
+            result[key] = this.documentToJson(value)
+        }
+    }
+    return result;
+}
 
 }
