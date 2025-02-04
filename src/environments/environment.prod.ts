db5f2a2e2310db6561b37fe358fc21720bd8fc61
victor yampolsky <tsanhan@gmail.com>
2025-02-04 17:14:48 +0200

    Remove legacy AndroidManifest.xml, update app name to 'mikveh4u', and clean up environment configuration by removing Firebase credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,4 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: 'AIzaSyDGPOUk4AVLY5hoCM0TVI1ZRAsf9Iy2nMw',
-    authDomain: 'mikveh-bo.firebaseapp.com',
-    projectId: 'mikveh-bo',
-    storageBucket: 'mikveh-bo.firebasestorage.app',
-    messagingSenderId: '136996094637',
-    appId: '1:136996094637:web:c8c1b0acb8da9f89d9fc24',
-    measurementId: '136996094637',
-  },
+  production: true
+  
 };
