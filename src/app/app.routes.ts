6bb436011fff77a1ca98cebd51a62e9a67c0f7d1
victor yampolsky <tsanhan@gmail.com>
2025-06-14 22:56:46 +0300

    Remove calendar and map components along with their associated files from the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,10 @@
 import { Routes } from '@angular/router';
 import { ApproachesComponent } from './pages/approaches/approaches.component';
-import { CalendarComponent } from './pages/calendar/calendar.component';
-import { MapComponent } from './pages/map/map.component';
 
 export const routes: Routes = [
   {
     path: 'approaches',
     component: ApproachesComponent
-  },
-  {
-    path: 'calendar',
-    component: CalendarComponent
-  },
-  {
-    path: 'map',
-    component: MapComponent
   },
   {
     path: '**',
