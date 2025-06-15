4f7ed7e5e1b142df6e8a0f76764c043f74783197
victor yampolsky <tsanhan@gmail.com>
2025-06-15 09:06:16 +0300

    Enhance calendar component: add locale and location imports; update date handling in onDateChange method

--- src/app/components/cal/cal.component.ts
+++ src/app/components/cal/cal.component.ts
@@ -5,10 +5,11 @@ import {
   OnInit,
 } from '@angular/core';
 import { IonDatetime } from '@ionic/angular/standalone';
-import { Zmanim } from '@hebcal/core';
+import { Zmanim , Locale, Location, HebrewDateEvent} from '@hebcal/core';
 import { EventsService } from 'src/app/services/events.service';
 import { AsyncPipe, DatePipe } from '@angular/common';
-
+import '@hebcal/cities';
+import { BehaviorSubject } from 'rxjs';
 @Component({
   selector: 'app-cal',
   templateUrl: './cal.component.html',
@@ -19,15 +20,36 @@ import { AsyncPipe, DatePipe } from '@angular/common';
 })
 export class CalComponent {
   events = inject(EventsService);
-  today = new Date().toISOString();
-
-  now = new Date();
-  israelTime = this.events.localISOString(this.now);
-
+  israelTime = this.events.localISOString(new Date());
+  selectedDateData = new BehaviorSubject<string>(this.dateToHebrew(new Date()));
   constructor() {}
 
   onDateChange(event: CustomEvent) {
     console.log('onDateChange:', event);
+    const date = new Date(event.detail.value);
+    // const loc = Location.lookup('Jerusalem') as Location;
+    // // let israelTime = this.events.localISOString(date);
+    // const zmanAwware = Zmanim.makeSunsetAwareHDate(loc, date, true);
+
+    // console.log('israelTime:', zmanAwware);
+    //     console.log('israelTime:', zmanAwware.render('he-x-NoNikud'));
+    //     const as = new HebrewDateEvent(zmanAwware);
+    //     console.log('israelTime:', as.render('he-x-NoNikud'));
+    date.setHours(20) // success!!!
+    console.log('selectedDate:', date);
+    console.log('israelTime:', this.dateToHebrew(date));
+    const test = "2025-06-15T19:30:22.733"
+    const dateTest = new Date(test);
+    console.log('israelTimeTest:', this.dateToHebrew(dateTest));
+
+
+  }
+
+  dateToHebrew(date: Date): string {
+    const loc = Location.lookup('Jerusalem') as Location;
+    const zmanAwware = Zmanim.makeSunsetAwareHDate(loc, date, true);
+    const as = new HebrewDateEvent(zmanAwware);
+    return as.render('he-x-NoNikud');
   }
 
 
