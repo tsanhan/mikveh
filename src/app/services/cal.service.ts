cdf65ab0a031f8cb378f5c8fa6df0d4da1d5eb02
vic <tsanhan@gmail.com>
2025-09-25 23:59:33 +0300

    Refactor CalComponent and CalService: comment out unused code in addEvent method, enhance event building logic, and improve clarity in date handling.

--- src/app/services/cal.service.ts
+++ src/app/services/cal.service.ts
@@ -6,6 +6,7 @@ import { CacheService } from './cache.service';
 import { Approach, ApproachName } from '../interfaces/approaches';
 import { ApproachService } from './approach.service';
 import { dateToHDate, getDateParam, hDateStringToHDate, hDateSunsetAwareStringToDate } from '../utils/date.util';
+import { HDate } from '@hebcal/core';
 
 @Injectable({
   providedIn: 'root',
@@ -32,31 +33,7 @@ export class CalService {
   async addEvent(date: Date, type: InputEventType, afterSunset: boolean) {
     afterSunset = type === InputEventType.HEFSEK_TAHARA ? false : afterSunset; // hefsek is always during the day
     const hdate = dateToHDate(date, afterSunset);
-    // const events: CachedCalEvent[] = this.cache.getCalEvents();
-    // const getTheBloodOnes: CachedCalEvent[] = events.filter(x => x.type === InputEventType.SEE_BLOOD);
 
-    // // 1. sfarad : if see blood during the first 4 days,
-    // if (type === InputEventType.SEE_BLOOD) {
-
-    //   let isTooClose = getTheBloodOnes.some(x => {
-    //     const subject = hDateStringToHDate(x.hDateSunsetAwareString);
-    //     const delta = hdate.deltaDays(subject);
-
-    //     switch (this.approach.approach$.getValue().name) {
-    //       case ApproachName.SEPHARDI:
-    //         return delta >= 0 && delta <= 3;
-    //       case ApproachName.CHABAD:
-    //       case ApproachName.ASHKENAZI:
-    //         return delta >= 0 && delta <= 4;
-    //       default:
-    //         return false;
-    //     }
-    //   });
-
-    //   if (isTooClose) {
-    //     return;
-    //   }
-    // }
 
     const event: CachedCalEvent = {
       type,
@@ -80,18 +57,28 @@ export class CalService {
 
     switch (type) {
       case InputEventType.SEE_BLOOD:
-        {
-          switch (approach.name) {
-            case ApproachName.ASHKENAZI:
-            case ApproachName.CHABAD:
-              followingEventsChabadOnaBenonit = this.buildEventsToHefsek(event, allevents, index);
-              break;
-            case ApproachName.SEPHARDI:
-              followingEventsChabadOnaBenonit = this.buildEventsToHefsek(event, allevents, index, 4);
-              break;
-          }
+        switch (approach.name) {
+          case ApproachName.ASHKENAZI:
+          case ApproachName.CHABAD:
+            followingEventsChabadOnaBenonit = this.buildEventsToHefsek(event, allevents, index);
+            break;
+          case ApproachName.SEPHARDI:
+            followingEventsChabadOnaBenonit = this.buildEventsToHefsek(event, allevents, index, 4);
+            break;
+
         }
-      break;
+        followingEventsChabadOnaBenonit.push(this.buildPrishaEventOnaBenonit(event, approach));
+        followingEventsChabadOnaBenonit.push(this.buildPrishaEventVesetHaHodesh(event, approach));
+        break;
+      case InputEventType.HEFSEK_TAHARA:
+        switch (approach.name) {
+          case ApproachName.CHABAD:
+          case ApproachName.ASHKENAZI:
+          case ApproachName.SEPHARDI:
+            followingEventsChabadOnaBenonit = this.buildEvents7CleanToPrisha(event, allevents, index);
+            break;
+        }
+
     }
 
     return [
@@ -99,23 +86,114 @@ export class CalService {
     ];
   }
 
+  private buildEvents7CleanToPrisha(event: CachedCalEvent, allevents: CachedCalEvent[], index: number): EventDto[] {
+    const { hDateSunsetAwareString, type, afterSunset, gregorianDateString } = event;
+    const rtn: EventDto[] = [];
+    const date: Date = hDateSunsetAwareStringToDate(hDateSunsetAwareString);
+    // add 1 after הפסק טהרה for ספירת 7 נקיים
+    rtn.push({
+      type: InputEventType.HEFSEK_TAHARA,
+      date: date.toISOString().split('T')[0],
+      textColor: '#a1a05cff',
+      backgroundColor: '#e6f0ff', // Light blue background
+      border: '1px solid #a1a05cff',
+      details: [
+        'הפסק טהרה',
+      ]
+    });
+    date.setDate(date.getDate() + 1);
+    for (let i = 1; i <= 7; i++) {
+      rtn.push({
+        type: DayType.SEVEN_CLEAN,
+        date: date.toISOString().split('T')[0],
+        textColor: '#3f4076ff',
+        backgroundColor: '#fff3e6', // Light orange background
+        border: '1px solid #8f9bddff',
+        details: [
+          `היום ה${i} של ספירת 7 נקיים`,
+        ]
+      })
+      date.setDate(date.getDate() + 1);
+    }
+
+
+    return rtn;
+  }
+
+  private buildPrishaEventVesetHaHodesh(event: CachedCalEvent, approach: Approach): EventDto{
+    const { hDateSunsetAwareString, type, afterSunset, gregorianDateString } = event;
+    let hDateVesetHaHodesh: HDate = hDateStringToHDate(hDateSunsetAwareString);
+    hDateVesetHaHodesh = hDateVesetHaHodesh.add(1, 'MONTHS')
+    const dateVesetHaHodesh = hDateVesetHaHodesh.greg();
+
+   
+    const rtn: EventDto = {
+        type: DayType.PRISHA,
+        date: dateVesetHaHodesh.toISOString().split('T')[0],
+        textColor: '#ff006aff',
+        border: '1px solid #ff006aff',
+        backgroundColor: '#ffe6e6', // Light red background
+        details: [
+          'פרישה - וסט החודש',
+        ]
+      }
+    ;
+    if (approach.name === ApproachName.ASHKENAZI) rtn.details.push('ראוי לחשוש עונה אחת לפני');
+
+    return rtn;
+  }
+
+  private buildPrishaEventOnaBenonit(event: CachedCalEvent, approach: Approach): EventDto {
+    const { hDateSunsetAwareString, type, afterSunset, gregorianDateString } = event;
+    const dateBenonit: Date = hDateSunsetAwareStringToDate(hDateSunsetAwareString);
+
+    switch (approach.name) {
+      case ApproachName.CHABAD:
+        dateBenonit.setDate(dateBenonit.getDate() + 30);
+        break;
+      case ApproachName.ASHKENAZI:
+      case ApproachName.SEPHARDI:
+        dateBenonit.setDate(dateBenonit.getDate() + 30);
+        if (afterSunset) dateBenonit.setDate(dateBenonit.getDate() - 1);
+
+        break;
+    }
+    const rtn: EventDto = 
+      {
+        type: DayType.PRISHA,
+        date: dateBenonit.toISOString().split('T')[0],
+        textColor: '#ff006aff',
+        border: '1px solid #ff006aff',
+        backgroundColor: '#ffe6e6', // Light red background
+        details: [
+          'פרישה - עונה בינונית',
+        ]
+      }
+
+    ;
+    if (approach.name === ApproachName.ASHKENAZI) rtn.details.push('ראוי לחשוש עונה אחת לפני');
+
+    return rtn;
+  }
 
   private buildEventsToHefsek(event: CachedCalEvent, allevents: CachedCalEvent[], index: number, numOfDays = 5): EventDto[] {
     const { hDateSunsetAwareString, type, afterSunset, gregorianDateString } = event;
-   
+
     const rtn: EventDto[] = [];
     const date: Date = hDateSunsetAwareStringToDate(hDateSunsetAwareString);
-
+    // prishaDate should be 30 days after 'date'
+    const prishaDate = new Date(date);
+    prishaDate.setDate(prishaDate.getDate() + 30);
     // מעיין פתוח
     for (let i = numOfDays; i > 0; i--) {
 
       rtn.push({
         type: DayType.MAAYAN_PATUAH,
         date: date.toISOString().split('T')[0],
-        textColor : '#ff0000', // Red
-        border : '1px solid #ff0000',
-        backgroundColor : '#ffe6e6', // Light red background
-        details : [
+        textColor: '#ff0000', // Red
+        border: '1px solid #ff0000',
+        backgroundColor: '#ffe6e6', // Light red background
+        details: [
           `עוד ${i} ימים אפשר להתחיל לבדוק הפסק טהרה`,
         ]
       })
@@ -132,6 +210,7 @@ export class CalService {
         'היום אפשר להתחיל לבדוק הפסק טהרה'
       ]
     });
+
 
     // add 1 after הפסק טהרה for ספירת 7 נקיים
     // for (let i = 1; i <= 7; i++) {
